7f7044ef48c00e1c2cffad12a6461d333e9cebc6
pasdom <shepherd93@live.it>
2025-12-10 16:54:34 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,20 @@
+// ========== Esteriorità ==========
+const DATA_E = [
+  { name: "Via Benedetto Bordone, 25", type:"Abitazione", lat:41.88146, lon:12.54130 },
+  { name: "Via Oreste Salomone, 7", type:"Abitazione", lat:41.87839, lon:12.54499 },
+  { name: "Via Carlo della Rocca, 30", type:"Abitazione", lat:41.87798, lon:12.54376 }
+];
+
+// ========== Interiorità ==========
+const DATA_I = [
+  { name: "Via Amedeo Cencelli, 23", type:"OM Hindu Mandir", lat:41.87898, lon:12.54314 },
+  { name: "Via Guido Cora, 21", type:"Tempio Hindu", lat:41.88140, lon:12.54107 },
+  { name: "Via Carlo della Rocca, 23", type:"Islam", lat:41.87845, lon:12.54417 }
+];
+
+// ========== Matrice ==========
+const DATA_M = [
+  { name: "Piazza Copernico, 10 La Torretta", type:"Residenziale", lat:41.88608, lon:12.53275 },
+  { name: "Via Pomponio Mela, 1", type:"Residenziale", lat:41.88598, lon:12.53205 },
+  { name: "Via Pomponio Mela, 2", type:"Residenziale", lat:41.88568, lon:12.53235 }
+];
